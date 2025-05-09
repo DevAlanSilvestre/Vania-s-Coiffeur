96f4632040e11c9b612634cf88b709d3e97a87c6
DevAlanSilvestre <martepraterra@outlook.com>
2025-05-09 19:38:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+let nota = 8;
+function avaliacao (){
+    
+    if (nota > 7); 
+
+ console.log ("Aluno aprovado");}  
+ 
+    else if (nota > 4 &&  < 7);{
+
+       console.log("Aluno reprovado")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
